93b147da4b48d5c37fafe0d846ef45015e2179cc
jedy <cocota93@gmail.com>
2020-11-04 09:46:35 +0900

    로깅 레벨 테스트

--- admin/src/main/java/org/jedy/home/controller/HomeController.java
+++ admin/src/main/java/org/jedy/home/controller/HomeController.java
@@ -1,15 +1,18 @@
 package org.jedy.home.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@Slf4j
 public class HomeController {
 
     @GetMapping(value = {"/", "/home"})
     public String home(){
+//        log.warn("hihi");
         return "home";
     }
 
